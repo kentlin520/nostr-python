41c89245073b1b350cf634fdb275874bd89e50f5
kentlin520 <asgh6542@gmail.com>
2023-05-09 02:18:58 +0800

    align code space format

--- client-sender.py
+++ client-sender.py
@@ -15,34 +15,33 @@ RELAY_Timeout_DURATION = 60
 
 
 
-
 async def send_message(host,messages):
-  uri = "wss://{}".format(host)
-  event = {
-      'id': None,
-      'sig': None,
-      'content':messages,
-      'created_at':None,
-      'kind':1,
-      'pubkey': None,
-      'tag':[]
-  }
+    uri = "wss://{}".format(host)
+    event = {
+        'id': None,
+        'sig': None,
+        'content':messages,
+        'created_at':None,
+        'kind':1,
+        'pubkey': None,
+        'tag':[]
+    }
 
-  data = [0,event['kind'],event['tag'],event['content']]
-  serialzed = json.dumps(data,separators=(',',':'),ensure_ascii=False).encode()
+    data = [0,event['kind'],event['tag'],event['content']]
+    serialzed = json.dumps(data,separators=(',',':'),ensure_ascii=False).encode()
 
-  async with websockets.connect(uri) as websocket:
-    payload = [
-        'EVENT',
-        event
-    ]
-    await websocket.send(json.dumps(payload))
+    async with websockets.connect(uri) as websocket:
+        payload = [
+            'EVENT',
+            event
+        ]
+        await websocket.send(json.dumps(payload))
 
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--host')
-  parser.add_argument('--message')
-  args = parser.parse_args()
-  asyncio.run(send_message(args.host,args.message))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host')
+    parser.add_argument('--message')
+    args = parser.parse_args()
+    asyncio.run(send_message(args.host,args.message))
